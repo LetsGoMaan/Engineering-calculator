598f7fbdcb37d5766890061ee2c6c90bb8591527
LetsGoMan <ilikeseik@gmail.com>
2024-03-12 14:09:30 +0300

    refactor: change file structure

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     devServer: {
         port: 8008,
